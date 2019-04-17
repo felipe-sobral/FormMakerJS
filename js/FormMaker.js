10979448940839f6e18c53686c8cda838b06a265
Felipe Sobral <krepper@outlook.com>
2019-04-17 03:34:51 -0300

    init2

--- js/FormMaker.js
+++ js/FormMaker.js
@@ -1,8 +1,8 @@
-var ForMaker = {
-    
-    newInput = function(){
+var FormMaker = function(id){
+    this.id = id;
 
+    this.print = function(html){
+        var formLocale =  document.getElementById(this.id);
+        formLocale.innerHTML = formLocale.innerHTML+html;
     }
-
 }
-
